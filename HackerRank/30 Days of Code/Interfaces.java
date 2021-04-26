efa6021a52aec05fbccc9401c7aab501b209a700
cashewna <65855420+cashewna@users.noreply.github.com>
2021-04-26 14:38:28 +0800

    added day 20

--- HackerRank/30 Days of Code/Interfaces.java
+++ HackerRank/30 Days of Code/Interfaces.java
@@ -22,17 +22,14 @@ public class Interfaces {
         }
     }
 
-    class Solution {
+    public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+    scan.close();
 
-        public static void main(String[] args) {
-            Scanner scan = new Scanner(System.in);
-            int n = scan.nextInt();
-            scan.close();
-
-            AdvancedArithmetic myCalculator = new Calculator();
-            int sum = myCalculator.divisorSum(n);
-            System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName() );
-            System.out.println(sum);
-        }
+    AdvancedArithmetic myCalculator = new Calculator();
+    int sum = myCalculator.divisorSum(n);
+    System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName() );
+    System.out.println(sum);
     }
 }
