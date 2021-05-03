d01bc70f7883bfd7669c83a7082fd54649808230
cashewna <krishnakhong@gmail.com>
2021-05-03 21:31:00 +0800

    Day 4: Class vs Instance

---
+++ HackerRank/30 Days of Code/ClassVsInstance.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+/**
+ * Day 4 <br>
+ * Create an instance of ClassVsInstance class in the main method. <br>
+ * If age < 13, you are young <br>
+ * If age >= 13 and < 18, you are a teenager <br>
+ * Else you are old.
+ */
+public class ClassVsInstance {
+  private int age;
+
+  public ClassVsInstance(int initialAge) {
+    // Add some more code to run some checks on initialAge
+    if (initialAge < 0) {
+      this.age = 0;
+      System.out.println("Age is not valid, setting age to 0.");
+    } else {
+      this.age = initialAge;
+    }
+  }
+
+  public void amIOld() {
+    // Write code determining if this person's age is old and print the correct statement:
+    if (age < 13) {
+      System.out.println("You are young.");
+    } else if (age < 18) {
+      System.out.println("You are a teenager.");
+    } else {
+      System.out.println("You are old.");
+    }
+  }
+
+  public void yearPasses() {
+    // Increment this person's age.
+    this.age += 1;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int T = sc.nextInt();
+    for (int i = 0; i < T; i++) {
+      int age = sc.nextInt();
+      ClassVsInstance p = new ClassVsInstance(age);
+      p.amIOld();
+      for (int j = 0; j < 3; j++) {
+        p.yearPasses();
+      }
+      p.amIOld();
+      System.out.println();
+    }
+    sc.close();
+  }
+}
