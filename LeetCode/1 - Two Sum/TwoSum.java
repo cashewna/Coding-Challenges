f6d0a16c290a8075979d39daea4d81a736bfe139
Krishna Khong <krishnakhong@gmail.com>
2021-05-15 20:47:51 +0800

    Initial commit

---
+++ LeetCode/1 - Two Sum/TwoSum.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+  public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+      int complement = target - nums[i];
+      if (map.containsKey(complement)) {
+        return new int[] {map.get(complement), i};
+      }
+      map.put(nums[i], i);
+    }
+    throw new IllegalArgumentException("No solution found.");
+  }
+
+  public static void main(String[] args) {
+    // Example input
+    int[] nums = {2, 7, 11, 15};
+    int target = 9;
+
+    TwoSum example = new TwoSum();
+    int[] answer = example.twoSum(nums, target);
+    for (int e : answer) {
+      System.out.print(e + " ");
+    }
+  }
+}
